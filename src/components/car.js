771958944138d4ba7da71126db6184343e6c724d
wuyuan <wuyuan@meizhitec.com>
2019-03-03 00:34:37 +0800

    修改

--- src/components/car.js
+++ src/components/car.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import carBody from '../img/car-body.png';
-import carFrontWheel from '../img/car-front-wheel.png';
-import carBackWheel from '../img/car-back-wheel.png';
+import React, { Component } from "react";
+import carBody from "../img/car-body.png";
+// import carFrontWheel from '../img/car-front-wheel.png';
+// import carBackWheel from '../img/car-back-wheel.png';
+import carWheel from "../img/car-wheel.png";
 
-export default function Car(){
-    return (
-        <div className="car">
-            <img id="car-front-wheel" src={carFrontWheel} alt="car-front-wheel"/>
-            <img id="car-back-wheel" src={carBackWheel} alt="car-back-wheel"/>
-            <img id="car-body" src={carBody} alt="car-body"/>
-        </div>
-    )
+export default function Car() {
+  return (
+    <div className="car">
+      <img id="car-front-wheel" src={carWheel} alt="car-front-wheel" />
+      <img id="car-back-wheel" src={carWheel} alt="car-back-wheel" />
+      <img id="car-body" src={carBody} alt="car-body" />
+    </div>
+  );
 }
